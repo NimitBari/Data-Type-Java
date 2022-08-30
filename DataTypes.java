7f833bca1da25400b85f685b18bc3245b258cca5
NimitBari <112305913+NimitBari@users.noreply.github.com>
2022-08-30 09:53:54 +0530

    initial comment

---
+++ DataTypes.java
@@ -0,0 +1,41 @@
+package JavaBasics;
+
+public class DataTypes {
+	
+	//Hi this is my first java code
+	
+	/*hi this
+	is my first
+	java code
+	and Im so happy*/
+	
+	//main()method--> startig/ execution point of the programe
+	public static void main(String[] args) {
+		
+		//1.int;
+		int i = 10;
+		System.out.println(i);
+		i = 20;
+		System.out.println(i);
+		int j = 30;
+		System.out.println(i+j);
+		System.out.println(j);
+		//2.double;
+		double d = 12.33;
+		
+		//3. char
+		char c = 'y';
+		//boolean
+		boolean b1 = true;
+		System.out.println(b1);
+		//string
+		String s = "Hello World";
+		System.out.println(s);
+		
+		
+		
+		
+
+	}
+
+}
